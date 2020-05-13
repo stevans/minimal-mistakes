0a6c91d1e752e8d539ab271dfbd18521976598bc
stevans <stevans@utexas.edu>
2020-05-13 13:05:26 -0500

    update census post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/test/",
         "teaser": null
+      },{
+        "title": "Using Census APIs to investigate the drop in youth-voter turnout bewteen 2016 and 2020",
+        "excerpt":"After Super Tuesday, young voters were frequently scapegoated by media outlets for Bernie Sanders’s loses in Super Tuesday contests like in this US Today article entitled, Many young voters sat out Super Tuesday, contributing to Bernie Sanders’ losses. Their reasoning was that a lot of polling before Super Tuesday showed...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/youth-vote/",
+        "teaser": null
       }]
