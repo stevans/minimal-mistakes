dea0fb62941ec192307f86a49bb8f06168b5db2d
stevans <stevans@utexas.edu>
2020-05-13 15:15:22 -0500

    remove test post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,12 +5,6 @@ var store = [{
         "url": "http://localhost:4000/energy-of-photons/",
         "teaser": null
       },{
-        "title": "Test Post",
-        "excerpt":"This is a test. This is only a test.   Header   A link is  here.                     A planet.            ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/test/",
-        "teaser": null
-      },{
         "title": "Using US Census APIs to investigate the drop in youth-voter turnout in 2020",
         "excerpt":"After Super Tuesday, young voters were frequently scapegoated by media outlets for Bernie Sanders’s loses in Super Tuesday contests like in this US Today article entitled, Many young voters sat out Super Tuesday, contributing to Bernie Sanders’ losses. Their reasoning was that a lot of polling before Super Tuesday showed...","categories": [],
         "tags": [],
